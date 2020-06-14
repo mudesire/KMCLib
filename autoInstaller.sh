91106f86fce084e5455bd76e251c0dec188d93bc
mudesire <muhiredesire01@gmail.com>
2020-06-14 22:38:01 +0200

    'Init'

---
+++ autoInstaller.sh
@@ -0,0 +1,47 @@
+# 
+#   Simple installer for unix 
+#    tested on ubuntu-Wsl
+#  Essential tools
+sudo apt-get update -y
+sudo apt-get install gedit  -y
+sudo apt-get install build-essential -y
+sudo apt-get install swig -y
+sudo apt-get install python-dev -y
+
+#  Make sure Python if in this path
+
+PYTHON27="/usr/include/python2.7"
+KMCLIB_PATH = pwd
+
+BUILDPATH = ${KMCLIB_PATH}/build
+
+EXTERNALS = ${KMCLIB_PATH}/c++/externals
+cd ${EXTERNALS} 
+make
+
+# If ok 
+cd ..
+
+mkdir ${BUILDPATH}
+cd ${BUILDPATH}
+cmake -Wno-dev ..
+
+make test.x
+./unittest/test.x
+make install
+
+# Modify ~/.bashrc and add 
+
+# copy et coller a la fin
+export PYTHONPATH=${KMCLIB_PATH}/python/src:${PYTHONPATH}
+
+# Test install 
+cd ${KMCLIB_PATH}
+
+python KMCLib/python/unittest/utest.py
+
+
+
+
+
+
